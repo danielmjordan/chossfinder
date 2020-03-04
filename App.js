6ef04f2865cc08ccdacc88391599eaf4cd1433ad
Daniel Jordan <danieljordan86@gmail.com>
2020-03-03 17:30:09 -0700

    Render routes based on geolocation

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Navigator from './routes/homeStack';
-import Search from './screens/search';
 import globalStyles from './styles/globalStyles';
 
 export default function App() {
-  const [ isLoading, setIsLoading ] = useState(true)
-  const [ location, setLocation ] = useState({})
-
-  useEffect(() => {
-    const getLocation = async () => {
-      const results = await navigator.geolocation.getCurrentPosition((data) => {
-        const { latitude, longitude} = data.coords;
-        setLocation({ lat: latitude, lon: longitude });
-        setIsLoading(false);
-      });
-    }
-    getLocation();
-  }, [])
-
-  if (isLoading) {
-    return (
-      <View style={globalStyles.loading}>
-        <ActivityIndicator size='large' color='#fff'/>
-      </View>
-    );
-  } else {
-    return (
-      <View style={globalStyles.container}>
-        <Navigator location={{location}}/>
-      </View>
-    );
-  }
+  return (
+    <View style={globalStyles.container}>
+      <Navigator />
+    </View>
+  );
 };
 
--- screens/home.js
+++ screens/home.js
@@ -1,57 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator, Button } from 'react-native';
 import Card from '../shared/card';
 import RoutePicker from '../shared/routePicker';
 import GradePicker from '../shared/gradePicker';
 import globalStyles from '../styles/globalStyles';
 import MOUNTAIN_PROJECT_API_KEY from '../private/keys';
 
-export default function Home ({ navigation, location }) {
+export default function Home ({ navigation }) {
   const [ routeResults, setRoutes ] = useState({});
-  const [ position, setPosition ] = useState(location);
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ location, setLocation ] = useState('');
   const [ difficulty, setDifficulty ] = useState({
     min: 5.12,
     max: 5.14
   })
 
   useEffect(() => {
-    (async () => {
-      try {
-        const res = await fetch(`https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=${location.lat}&lon=-${location.lon}&maxDistance=10&minDiff=${difficulty.min}&maxDiff=${difficulty.max}&key=${MOUNTAIN_PROJECT_API_KEY}`);
-        const results = await res.json();
-        setRoutes(results);
-      } catch (err) {
-        return err;
-      }
-    })();
-  }, []);
+    if (!location) {
+      getLocation();
+    } else {
+      getRoutes();
+    }
+  }, [location]);
 
-  console.log(location);
+  const getLocation = async () => {
+    const results = await navigator.geolocation.getCurrentPosition((data) => {
+      const { latitude, longitude} = data.coords;
+      setLocation({ lat: latitude, lon: longitude });
+    });
+  }
+
+  const getRoutes = async () => {
+      const res = await fetch(
+        `https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=${location.lat}&lon=${location.lon}&maxDistance=10&maxResults=100&minDiff=${difficulty.min}&maxDiff=${difficulty.max}&key=${MOUNTAIN_PROJECT_API_KEY}`
+      );
+      const results = await res.json();
+      setRoutes(results);
+      setIsLoading(false);
+  };
 
   const pressHandler = () => {
     navigation.navigate('Routes', routeResults);
-  }
+  };
 
-  return (
-    <View>
-      <Card>
-        <Button
-          title="Get Routes for Current Location"
-          onPress={pressHandler}
-        />
-      </Card>
-      <Card>
-        <Button
-          title="Search for Routes"
-          onPress={() => console.log("navigate to search screen")}
-        />
-      </Card>
-      {/* <Card>
-        <RoutePicker position={position} setPosition={setPosition}/>
-      </Card>
-      <Card>
-        <GradePicker difficulty={difficulty} setDifficulty={setDifficulty} />
-      </Card> */}
-    </View>
-  );
+  if (isLoading) {
+    return (
+      <View style={globalStyles.loading}>
+        <ActivityIndicator size='large' color='#fff'/>
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <Card>
+          <Button
+            title={'Get Routes for Current Location'}
+            onPress={pressHandler}
+          />
+        </Card>
+        <Card>
+          <Button
+            title={"Search for Routes"}
+            onPress={() => console.log("navigate to search screen")}
+          />
+        </Card>
+        {/* <Card>
+          <RoutePicker location={location} setPosition={setLocation}/>
+        </Card>
+        <Card>
+          <GradePicker difficulty={difficulty} setDifficulty={setDifficulty} />
+        </Card> */}
+      </View>
+    );
+  }
 }
